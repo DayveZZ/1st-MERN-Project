df926c25cacfad39f29e7d41b1a88da9bdb304ba
DayveZZ <abhijitdebofficial@gmail.com>
2025-08-28 20:53:25 +0530

    commit

--- Backend/express.js
+++ Backend/express.js
@@ -4,13 +4,3 @@ const cors = require("cors");
 const userData = require("../MOCK_DATA.json");
 
 const allowOrigin = ["http://localhost:5173"];
-
-const corsOptions = {
-  origin: function (origin, callback) {
-    if (allowOrigin.indexOf(origin) !== -1 || !origin) {
-      callback(null, true);
-    } else {
-      callback(new Error("Not allowed by CORS"));
-    }
-  },
-};
