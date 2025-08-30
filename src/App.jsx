e19b965a602aebb981568a3af86de86753ca2ba9
DayveZZ <abhijitdebofficial@gmail.com>
2025-08-30 20:21:10 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div className="flex flex-col justify-center items-center h-screen">
+        <h1>1ST MERN PROJECT</h1>
+
+        <ul className="p-2 border rounded mt-8">
+          <li className="text-center mb-2">NAME CARD</li>
+          <li>ID: User ID</li>
+          <li>
+            NAME: <span>First Name</span>
+            <span> Last Name</span>
+          </li>
+          <li>EMAIL: Email Address</li>
+        </ul>
+      </div>
+    </>
+  );
+}
+
+export default App;
